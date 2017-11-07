b9a337ec2fe6b0a0f171da1ea4947a11496a838e
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-11-07 19:35:14 +0000

    fix utility script to preserve entry contents and use correct limits

--- utils/scripts/fill-the-gap-data.sh
+++ utils/scripts/fill-the-gap-data.sh
@@ -6,5 +6,5 @@ ENTRIES=$1
 [[ -z $2 ]] && echo "missing report file location" && exit 1
 REPORT_FILE=$2
 
-awk -v N=$ENTRIES '{ a[$i]=1; next; } END{ for(i=2; i<N; i+=4) if(a[i]) print 1; else print 0; }' "$REPORT_FILE"
+awk -v N=$ENTRIES '{ a[$1]=$2; } END{ for(i=2; i<=N; i+=4) if(a[i]) print a[i]; else print 0; }' "$REPORT_FILE"
 
